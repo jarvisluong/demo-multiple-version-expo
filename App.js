487d562c4d5149fbfd0d40a209ccfb4256941f07
Luong Dang Hai <hailuongdang1998@gmail.com>
2019-01-17 21:19:30 +0200

    adding build variants to demo project

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Constants } from "expo";
+
+const variant = Constants.manifest.extra.variant;
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>You are running in {variant} mode!</Text>
       </View>
     );
   }
@@ -14,8 +18,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
